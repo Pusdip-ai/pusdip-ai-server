3f757aca0029bfb1b8731d2e22e61818add4c331
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-08-11 20:41:20 +0700

    Create Article model

---
+++ src/models/Article.ts
@@ -0,0 +1,37 @@
+export enum ArticleSource {
+  Scopus = "scopus",
+  Entrez = "entrez",
+  DOAJ = "DOAJ",
+}
+
+export interface IArticle {
+  info: {
+    authors: string[];
+    title: string;
+    abstract: string;
+    journal: {
+      title: string;
+      volume?: number;
+      issue?: number;
+      year: number;
+    };
+    date: {
+      year: number;
+      day?: number;
+      month?: number;
+    };
+    isOpenAccess?: boolean;
+  };
+  links?: {
+    doi?: string;
+    url?: string;
+    source?: ArticleSource;
+  };
+}
+
+export class Article implements IArticle {
+  constructor(
+    public info: IArticle["info"],
+    public links: IArticle["links"]
+  ) {}
+}
