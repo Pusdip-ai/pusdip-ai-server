ad741aa2dfd7d683840466a03ff571acd8b13166
Reyhan Ardiya <mreyhanapwsw@gmail.com>
2024-08-13 09:25:12 +0700

    Change getting port & host var

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,18 @@ import articlesRouter from "./routers/articles";
 import bodyParser from "body-parser";
 import puschatRouter from "./routers/puschat";
 
-// import logger from "pino-http";
+const PORT = process.env.PORT;
+const HOST = process.env.HOST;
+
+if (!PORT) {
+  logger.error("No port specified");
+  process.exit(1);
+}
+
+if (!HOST) {
+  logger.error("No host specified");
+  process.exit(1);
+}
 
 // Create an Express application
 const app = express();
@@ -15,15 +26,11 @@ app.use(pinoHTTPMiddleware);
 app.use(cors());
 app.use(bodyParser.json());
 
-// Define a port from environment variables or default to 3000
-const PORT = process.env.PORT ? +process.env.PORT : 3000;
-const HOST = process.env.HOST || "localhost";
-
 // Add routers
 app.use("/articles", articlesRouter);
 app.use("/puschat", puschatRouter);
 
 // Start the server
-app.listen(PORT, HOST, () => {
+app.listen(+PORT, HOST, () => {
   logger.info(`Server is running on http://localhost:${PORT}`);
 });
